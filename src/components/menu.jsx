b757ae1a604cf61daa977bfb35463fc8fe36cede
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-11 04:47:55 +0500

    Pizza menu styling done

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,12 +1,26 @@
+import Pizza from "./pizza";
+import pizzaData from "../../public/data";
 function Menu() {
+  const numOfPizzas = pizzaData.length;
   return (
-    <div>
-      <h1>OUR MENU</h1>
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven, all organic, all delicious.
-      </p>
-    </div>
+    <main className="menu">
+      <h2>OUR MENU</h2>
+      {numOfPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are still working on our Menu. Please come back Later :)</p>
+      )}
+    </main>
   );
 }
 export default Menu;
