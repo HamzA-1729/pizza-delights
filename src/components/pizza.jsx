429be19877658fed50be9e705b7452c0c96ce411
Hamza Sajid <150107585+sams658@users.noreply.github.com>
2024-07-11 04:49:09 +0500

    Styling,data fetching and prop validation issue resolved

--- src/components/pizza.jsx
+++ src/components/pizza.jsx
@@ -1,10 +1,26 @@
-function Pizza() {
+import PropTypes from "prop-types";
+function Pizza({ pizzaObj }) {
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
+    </li>
   );
 }
+
+// Define prop types for the Pizza component
+Pizza.propTypes = {
+  pizzaObj: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    ingredients: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    photoName: PropTypes.string.isRequired,
+    soldOut: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
 export default Pizza;
